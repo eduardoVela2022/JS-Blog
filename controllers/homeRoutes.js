aa096d402ea58fa1ac98dbfd98938dd65e405be9
eduardoVela <eduardovela2022@icloud.com>
2024-07-10 15:17:49 -0600

    Added the dashboard and post view.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 // Imports
 const router = require("express").Router();
-const { Post, User } = require("../models");
+const { Post, User, Comment } = require("../models");
 
 // This route is for the homepage view
 router.get("/", async (req, res) => {
@@ -14,6 +14,47 @@ router.get("/", async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Renders the homepage view with the obtained data
+    res.status(200).json(posts);
+  } catch (err) {
+    // Send error status, if something went wrong
+    res.status(500).json(err);
+  }
+});
+
+// This route is for the dashboard view
+router.get("/dashboard", async (req, res) => {
+  try {
+    // Gets all the posts from the database that contain the logged in user's id
+    const postData = await Post.findAll({
+      where: { userId: req.session.user_id },
+    });
+
+    // Converts the obtained posts into plain objects
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Renders the dashboard view with the obtained data
+    res.status(200).json(posts);
+  } catch (err) {
+    // Send error status, if something went wrong
+    res.status(500).json(err);
+  }
+});
+
+// This route is for the post view
+router.get("/post/:id", async (req, res) => {
+  try {
+    // Gets the post with the given id, along with the username of the user that created it and its comments
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User, attributes: ["username"] },
+        { model: Comment, attributes: ["content"] },
+      ],
+    });
+
+    // Converts the obtained posts into plain objects
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Renders the post view with the obtained data
     res.status(200).json(posts);
   } catch (err) {
     // Send error status, if something went wrong
