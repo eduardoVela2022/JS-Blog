49f2fe03dc958bb06fd03e090cd4430f3e2ad7cd
eduardoVela <eduardovela2022@icloud.com>
2024-07-12 13:36:33 -0600

    Added style to the dashboard view.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,14 +26,14 @@ router.get("/dashboard", async (req, res) => {
   try {
     // Gets all the posts from the database that contain the logged in user's id
     const postData = await Post.findAll({
-      where: { userId: req.session.user_id },
+      where: { userId: 1 },
     });
 
     // Converts the obtained posts into plain objects
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Renders the dashboard view with the obtained data
-    res.status(200).json(posts);
+    res.render("dashboard", { posts });
   } catch (err) {
     // Send error status, if something went wrong
     res.status(500).json(err);
